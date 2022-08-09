8d9f7e15ae889ac2d2503aa3f1882dc23e47faf5
Adnan Khurshid <adnankhurshid251@gmail.com>
2022-08-09 09:32:07 +0530

    responsive burger

--- js/main.js
+++ js/main.js
@@ -31,18 +31,7 @@ const onscroll = (el, listener) => {
 /**
  * Toggle .header-scrolled class to #header when page is scrolled
  */
-let selectHeader = select("#header");
-if (selectHeader) {
-    const headerScrolled = () => {
-        if (window.scrollY > 100) {
-            selectHeader.classList.add("header-scrolled");
-        } else {
-            selectHeader.classList.remove("header-scrolled");
-        }
-    };
-    window.addEventListener("load", headerScrolled);
-    onscroll(document, headerScrolled);
-}
+
 
 const navSlide = () => {
     const burger = document.querySelector(".burger");
@@ -65,55 +54,47 @@ const navSlide = () => {
     });
 };
 
-//Div Highlight//////////////////////
-
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll(".main-nav-link");
-window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
-        }
-    });
-
-    navLi.forEach((li) => {
-        li.classList.remove("btn-cta");
-        if (li.classList.contains(current)) {
-            li.classList.add("btn-cta");
-        }
-    });
-});
-
 navSlide();
 
-console.log(scrollY);
 
-//Mobile viewport search tab fix//////////////////////////
-// save old window size to adjust only if width changed
-let oldWidth = window.innerWidth,
-    oldHeight = window.innerHeight;
-// element to adjust
-const target = document.querySelector(".vh100");
-// adjust the size if window was resized
-window.addEventListener("resize", handleResize);
+const modalOperation = () => {
+    const signupbtn = document.getElementById("signup");
+    const loginbtn = document.getElementById("login");
+    const signupModal = document.getElementById("signup-modal");
+    const loginModal = document.getElementById("login-modal");
+    const closeSignup = document.getElementById("close-signup");
+    const closeLogin = document.getElementById("close-login");
 
-function handleResize(initial = false) {
-    // the parameter is used for calling the function on page load
-    /*
-     * if the width changed then resize
-     * without this Chrome mobile resizes every time navbar is hidden
-     */
-    if (window.innerWidth !== oldWidth || initial) {
-        // stretch the target
-        target.classList.add("setting-100vh");
-        // save height and apply as min height
-        const h = target.clientHeight;
-        target.classList.remove("setting-100vh");
-        target.style.minHeight = h + "px";
-    }
+    signupbtn.addEventListener("click", () => {
+        signupModal.classList.toggle("hidden");
+    });
+
+    loginbtn.addEventListener("click", () => {
+        loginModal.classList.toggle("hidden");
+    });
+
+    closeSignup.addEventListener("click", () => {
+        signupModal.classList.add("hidden");
+    });
+
+    closeLogin.addEventListener("click", () => {
+        loginModal.classList.add("hidden");
+    });
+
 }
-// call when page is loaded
-handleResize(true);
+
+modalOperation();
+
+
+
+
+const alertOperation = () => {
+    const alert = document.querySelector(".alert");
+    const closeAlert = document.getElementById("close-alert");
+
+    closeAlert.addEventListener("click", () => {
+        alert.classList.add("hidden");
+    });
+}
+
+alertOperation();
